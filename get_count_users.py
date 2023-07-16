7bf2833b86fce62a3d0e0184b2cf27b56bf16e05
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-16 23:50:15 +0500

    solve main

--- get_count_users.py
+++ get_count_users.py
@@ -1,6 +1,8 @@
-import get_data
 
-def get_count_users(data:dict) -> int:
+import get_data
+import json
+
+def get_count_users(data: dict) -> int:
     """
     You are given dictionary. Find the number of users.
 
@@ -9,4 +11,12 @@ def get_count_users(data:dict) -> int:
     Returns:
         int: number of users
     """
+    sum = 0
+    for user in data['results']:
+        sum+=1
+    return sum
+
+data_json = open('randomuser_data.json','r').read()
+data = json.loads(data_json)
+print(get_count_users(data))
     
